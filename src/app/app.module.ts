2fa61e697e8453e6f65428182f5725742edd6658
Iotaslair <35147738+Iotaslair@users.noreply.github.com>
2020-04-10 22:24:10 -0400

    Cleared out app-default and added bootstrap page to the home page and seperated it into components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppComponent } from './app.component';
-import { HomePageComponent } from './components/home-page/home-page.component';
+import { HomePageComponent} from './components/home-page/home-page.component';
+import { LandingComponent } from './components/home-page/landing/landing.component';
+import { AboutComponent } from './components/home-page/about/about.component';
+import { ProjectsComponent } from './components/home-page/projects/projects.component';
+import { FooterComponent } from './components/home-page/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HomePageComponent
+    HomePageComponent,
+    LandingComponent,
+    AboutComponent,
+    ProjectsComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [HomePageComponent]
 })
 export class AppModule { }
